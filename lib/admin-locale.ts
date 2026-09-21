41fdd5028f333325709e8229ee99318f0d4f68e6
alharamainelite-1 <alharamainelite@gmail.com>
2026-09-22 00:14:05 +0300

    Add server-only admin locale reader

---
+++ lib/admin-locale.ts
@@ -0,0 +1,5 @@
+import {cookies} from 'next/headers';
+export type AdminLocale='en'|'ar';
+export async function getAdminLocale():Promise<AdminLocale>{
+  return (await cookies()).get('he_locale')?.value==='ar'?'ar':'en';
+}
